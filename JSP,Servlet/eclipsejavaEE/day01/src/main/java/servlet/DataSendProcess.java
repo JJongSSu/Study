c8c07a8923ab0604a0bb3ec52f34a4340b94e233
JJongSSu <dlwnsrl8964@gmail.com>
2022-10-04 15:11:20 +0900

    first commit

--- JSP,Servlet/eclipsejavaEE/day01/src/main/java/servlet/DataSendProcess.java
+++ JSP,Servlet/eclipsejavaEE/day01/src/main/java/servlet/DataSendProcess.java
@@ -10,16 +10,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class HelloWorld
+ * Servlet implementation class DataSendProcess
  */
-@WebServlet(description = "My first servlet", urlPatterns = { "/HelloWorld" })
-public class HelloWorld extends HttpServlet {
+@WebServlet("/dataProcess")
+public class DataSendProcess extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public HelloWorld() {
+    public DataSendProcess() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -30,18 +30,14 @@ public class HelloWorld extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 //		response.getWriter().append("Served at: ").append(request.getContextPath());
-//		HTML 문서는 utf-8로 encoding 지정, 웹브라우저에서 한글 표시할 때 인코딩 방식 지정(한글 인식 못할 때)
-		response.setContentType("text/html;charset=utf-8");
+		
+		// 전송data 가져오기
+		String data = request.getParameter("data");
+		
+		response.setContentType("text/html; charset=utf-8");
 		
 		PrintWriter out = response.getWriter();
-		out.print("<h3>Hello World</h3>");
-		out.print("헬로우 월드");
-		out.print("<br><img src='./Image/zzanggu.png'>");
-		out.print("<br><img src='./Image/Zzanggu.jpg'>");
-		
-//		data를 가공한 후에 메모리에서 찍어보는 과정, data의 흐름을 읽을 때 사용
-		System.out.println("ContextPath: " + request.getContextPath());
-		System.out.println("requestURL: " + request.getRequestURL());
+		out.print("전송 data는 " + data + "입니다.");
 		
 	}
 
--- JSP,Servlet/eclipsejavaEE/day01/src/main/java/servlet/ex2.java
+++ JSP,Servlet/eclipsejavaEE/day01/src/main/java/servlet/ex2.java
@@ -10,16 +10,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class HelloWorld
+ * Servlet implementation class ex2
  */
-@WebServlet(description = "My first servlet", urlPatterns = { "/HelloWorld" })
-public class HelloWorld extends HttpServlet {
+@WebServlet("/ex2")
+public class ex2 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public HelloWorld() {
+    public ex2() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -30,18 +30,15 @@ public class HelloWorld extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 //		response.getWriter().append("Served at: ").append(request.getContextPath());
-//		HTML 문서는 utf-8로 encoding 지정, 웹브라우저에서 한글 표시할 때 인코딩 방식 지정(한글 인식 못할 때)
+		
 		response.setContentType("text/html;charset=utf-8");
 		
 		PrintWriter out = response.getWriter();
-		out.print("<h3>Hello World</h3>");
-		out.print("헬로우 월드");
-		out.print("<br><img src='./Image/zzanggu.png'>");
-		out.print("<br><img src='./Image/Zzanggu.jpg'>");
-		
-//		data를 가공한 후에 메모리에서 찍어보는 과정, data의 흐름을 읽을 때 사용
-		System.out.println("ContextPath: " + request.getContextPath());
-		System.out.println("requestURL: " + request.getRequestURL());
+		out.print("<html>");
+		out.print("<body>");
+		out.print("이예지");
+		out.print("</body>");
+		out.print("</html>");
 		
 	}
 
