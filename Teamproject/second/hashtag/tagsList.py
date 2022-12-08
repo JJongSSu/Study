363a1dfb2783233c57e99d4616a111786c332a5a
JJongSSu <112380397+JJongSSu@users.noreply.github.com>
2022-12-08 15:05:15 +0900

    first commit

---
+++ Teamproject/second/hashtag/tagsList.py
@@ -0,0 +1,21 @@
+Tags = {
+	"common": [
+		'샴페인', '부모님', '가족', '지인', '친구', '청량감', '전', '고급스럽다', '인생', '특별하다'
+	],
+
+	"android": [
+		"android", "smartPhone", "androidStudio", "studio", "java", "mobile",
+		"mobileApplication", "mobileDeveloper", "google", "googleDevelopers", 
+		"androidDevelopent", "UX", "UI"
+	],
+
+	"web": [
+		"web", "webDevelopment", "frontEnd", "backEnd", "website", "webapp",
+		"cloud", "hosting", "webDeveloper", "database", "html", "css", "javascript",
+		"html5", "js", "webdesign", "webdesigner", "internet", "IT", "data", "UX", "UI"
+	],
+
+	"raspberrypi": [
+		"raspberrypi", "pi", "hardware", "microcontroller", "IoT", "automation"
+	]
+}
