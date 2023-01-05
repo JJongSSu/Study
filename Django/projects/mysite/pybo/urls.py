0e1e9c42f4cd841fca79462ed76c004e21d58fcb
JJongSSu <112380397+JJongSSu@users.noreply.github.com>
2023-01-05 17:19:13 +0900

    first commit

--- Django/projects/mysite/pybo/urls.py
+++ Django/projects/mysite/pybo/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+# 네임스페이스 추가
+app_name = 'pybo'
+
 urlpatterns = [
-    path('',views.index),
+    path('',views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'), # question_id가 정수이기 때문에 int로 작성
 ]
--- Django/projects/mysite/pybo/views.py
+++ Django/projects/mysite/pybo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from django.http import HttpResponse
@@ -10,5 +10,12 @@ def index(request):
     context = {'question_list':question_list}
     return render(request,'pybo/question_list.html', context)
     # return HttpResponse("환영합니다.")
+
+def detail(request, question_id):
+    # pybo 내용 출력
+    # question = Question.objects.get(id=question_id)
+    question = get_object_or_404(Question, pk=question_id)  # 있는 건 보여주고 없으면 오류 페이지 보여줌
+    context = {'question':question}
+    return render(request, 'pybo/question_detail.html', context)
 
 
