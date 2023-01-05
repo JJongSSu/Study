f332b338a7d0ad37358f1aab4240d0122a70e5c2
JJongSSu <112380397+JJongSSu@users.noreply.github.com>
2023-01-05 12:31:15 +0900

    first commit

--- Django/projects/mysite/pybo/views.py
+++ Django/projects/mysite/pybo/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Question
+
 def index(request):
-    return HttpResponse("환영합니다.")
+    # pybo 목록 출력
+    question_list = Question.objects.order_by('-create_date')   # 내림차순 정렬
+    context = {'question_list':question_list}
+    return render(request,'pybo/question_list.html', context)
+    # return HttpResponse("환영합니다.")
+
 
