e1c1fc374155d014888057a0999dfacd2139d52c
JJongSSu <dlwnsrl8964@gmail.com>
2024-08-28 12:33:43 +0900

    + nomad code ts study

---
+++ Nomadcoder/typechain/src/myPackage.js
@@ -0,0 +1,20 @@
+// @ts-check
+/**
+ * Initalizes the project
+ * @param {object} config 
+ * @param {boolean} config.debug
+ * @param {string} config.url
+ * @returns {boolean}
+ */
+export function init(config){
+    return true;
+}
+
+/**
+ * 
+ * @param {number} code 
+ * @returns 
+ */
+export function exit(code){
+    return code + 1;
+}
